de58af2caa8fbcc569c86e9fa4f4f1a5790bb8fa
Gabriel Luis Freitas <gabiluisfreitas@gmail.com>
2023-03-26 19:43:14 +0100

    output cleared on jnic snowvi

--- SNOWVi/21_jnic_x86/SNOWVi_x86.c
+++ SNOWVi/21_jnic_x86/SNOWVi_x86.c
@@ -235,9 +235,6 @@ int main()
   }
   time_improve = clock() - time_improve;
 
-  printf("Original: %f\n", (double)time_original);
-  printf("Improved: %f\n", (double)time_improve);
-
-  printf("Speedup: %f%%\n", (((double)time_original / (double)time_improve) - 1.0) * 100.0);
+  printf("Speedup: %f %%\n", (((double)time_original / (double)time_improve) - 1.0) * 100.0);
   return 0;
 }
